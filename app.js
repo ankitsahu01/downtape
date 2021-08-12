824af502404ccd3bfda86f89479cbb83e8780a82
Ankit Sahu <sahu6856@gmail.com>
2021-08-12 23:08:55 +0530

    update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use('/api/youtube',require('./routers/YoutubeRoutes'));
 
 if(process.env.NODE_ENV==="production"){
     const path = require('path');
-    app.get('/*',(req,res)=>{
+    app.get('/',(req,res)=>{
         app.use(express.static(path.resolve(__dirname,'client','build')));
         res.sendFile(path.resolve(__dirname,'client','build','index.html'));
     });
