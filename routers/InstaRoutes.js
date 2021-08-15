e68cece9bb1e79d8d3c864a27bafba26d2f51a67
Ankit Sahu <sahu6856@gmail.com>
2021-08-15 20:50:54 +0530

    test

--- routers/InstaRoutes.js
+++ routers/InstaRoutes.js
@@ -4,16 +4,24 @@ const axios = require("axios");
 const download = require('image-downloader')
 const cheerio = require('cheerio');
 
-
+const { parse } = require("node-html-parser");
 
 const getInfo = async (url) => {
-  console.log(url);
   const html = await axios.get(url);
-  const $ = cheerio.load(html.data);
-  const videoString = $("meta[property='og:video']").attr("content");
-  const imgString = $("meta[property='og:image']").attr("content");
-  const caption = $("meta[property='og:title']").attr("content");
-  return {videoString, imgString, caption};
+  const root = parse(html.data);
+  // console.log(html);
+  
+
+  let caption2=root.querySelector('.Caption')?.text;
+  console.log(caption2);
+
+  return html;
+
+  // const $ = cheerio.load(html.data);
+  // const videoString = $("meta[property='og:video']").attr("content");
+  // const imgString = $("meta[property='og:image']").attr("content");
+  // const caption = $("meta[property='og:title']").attr("content");
+  // return {videoString, imgString, caption};
 };
 
 router.get('/info', async (req, res)=>{
@@ -22,16 +30,8 @@ router.get('/info', async (req, res)=>{
         // url= url.replace('?utm_medium=copy_link','');
         const data = await getInfo(url);
         console.log(data);
-        res.json(data);
-        // const {videoString, imgString, caption} = await getInfo(url);
-        // const options= { url: imgString, dest: './backend_img' }
-        // const resp= await download.image(options);
-        // const imgName= resp.filename.replace('backend_img\\','');
-        // if (videoString !== undefined) {
-        //   res.status(200).json({ videoLink:videoString, imgName, caption });
-        // }else{
-        //   res.status(404).json({ error: "Invalid Link!" });
-        // }
+
+        res.json("ok");
     }catch(err){
         console.log(err.message);
         res.status(404).send(err.message);
