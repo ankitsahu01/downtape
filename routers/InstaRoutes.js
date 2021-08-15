b12abfd51a14832fea3ee6a2d51902778522c9c0
Ankit Sahu <sahu6856@gmail.com>
2021-08-15 20:27:11 +0530

    test

--- routers/InstaRoutes.js
+++ routers/InstaRoutes.js
@@ -4,50 +4,6 @@ const axios = require("axios");
 const download = require('image-downloader')
 const cheerio = require('cheerio');
 
-const { parse } = require("node-html-parser");
-
-async function getPostLink(url) {
-  url = url + 'embed' + '/captioned';
-  let res = axios.get(url).then(async (response) => {
-    const root = parse(response.data);
-    let videoLink = getVideoLinkFromHtml(response.data);
-    while (videoLink.search("&amp;") != -1) {
-      videoLink = videoLink.replace("&amp;", "&");
-    }
-    let caption =await getCaptionFromHtml(response.data);
-    let img = root.querySelector('img.EmbeddedMediaImage').getAttribute("src");
-    return {videoLink, caption, img};
-  });
-  return res;
-}
-
-async function getCaptionFromHtml(html) {
-  const root = parse(html);
-  let caption=root.querySelector('.Caption')?.text;
-  if(caption == undefined)
-      caption="No caption";
-  caption=caption.replace("view all comments","");
-  return caption;
-}
-
-function getVideoLinkFromHtml(html) {
-  let crop = "{\"" + html.substring(html.search("video_url"), html.search("video_url") + 1000);
-  crop = crop.substring(0, crop.search(",")) + "}";
-  return JSON.parse(crop).video_url;
-}
-
-router.get('/test', async (req, res)=>{
-    try{
-        let url = req.query.url;
-        url= url.replace('?utm_medium=copy_link','');
-        const data= await getPostLink(url);
-        console.log(data);
-        res.json(data);
-    }catch(err){
-        console.log(err.message);
-        res.status(404).send(err.message);
-    }
-});
 
 
 const getInfo = async (url) => {
@@ -64,7 +20,6 @@ router.get('/info', async (req, res)=>{
     try{
         let url = req.query.url;
         // url= url.replace('?utm_medium=copy_link','');
-        url= url + 'embed' + '/captioned';
         const data = await getInfo(url);
         console.log(data);
         res.json(data);
