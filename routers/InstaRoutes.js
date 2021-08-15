d2dc363186e9a1a7b2e19fe02e9bee0a34f81620
Ankit Sahu <sahu6856@gmail.com>
2021-08-15 18:45:09 +0530

    test

--- routers/InstaRoutes.js
+++ routers/InstaRoutes.js
@@ -4,12 +4,36 @@ const axios = require("axios");
 const download = require('image-downloader')
 const cheerio = require('cheerio');
 
+// router.get('/info', async (req, res)=>{
+//   try{
+//       let url = req.query.url;
+//       url= url.replace('?utm_medium=copy_link','');
+      
+      
+//       const {videoString, imgString, caption} = await getVideo(url);
+
+//       console.log(videoString, imgString, caption);
+//       const options= { url: imgString, dest: './backend_img' }
+//       const resp= await download.image(options);
+//       const imgName= resp.filename.replace('backend_img\\','');
+//       if (videoString !== undefined) {
+//         res.status(200).json({ videoLink:videoString, imgName, caption });
+//       }else{
+//         res.status(404).json({ error: "Invalid Link!" });
+//       }
+//   }catch(err){
+//       console.log(err.message);
+//       res.status(404).send(err.message);
+//   }
+// });
+
 
 const getVideo = async (url) => {
   const html = await axios.get(url);
   const $ = cheerio.load(html.data);
-  console.log($);
   const videoString = $("meta[property='og:video']").attr("content");
+  console.log($("meta[property='og:video']"));
+  console.log($("meta[property='og:video']").attr("content"));
   const imgString = $("meta[property='og:image']").attr("content");
   const caption = $("meta[property='og:title']").attr("content");
   return {videoString, imgString, caption};
@@ -19,9 +43,7 @@ router.get('/info', async (req, res)=>{
     try{
         let url = req.query.url;
         url= url.replace('?utm_medium=copy_link','');
-        console.log(url);
         const {videoString, imgString, caption} = await getVideo(url);
-        console.log(videoString, imgString, caption);
         const options= { url: imgString, dest: './backend_img' }
         const resp= await download.image(options);
         const imgName= resp.filename.replace('backend_img\\','');
