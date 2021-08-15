619cd1936128a3d13a2c7006e93ca931bd46063e
Ankit Sahu <sahu6856@gmail.com>
2021-08-15 20:06:44 +0530

    test

--- routers/InstaRoutes.js
+++ routers/InstaRoutes.js
@@ -6,51 +6,48 @@ const cheerio = require('cheerio');
 
 const { parse } = require("node-html-parser");
 
-// async function getPostLink(url) {
-//   // url = url + 'embed' + '/captioned';
-//   let res = axios.get(url).then(async (response) => {
-//     const root = parse(response.data);
-//     let link = "";
-//     if (response.data.search("video_url") != -1)
-//       link = getVideoLinkFromHtml(response.data);
-//     else
-//       link = root.querySelector('img.EmbeddedMediaImage').getAttribute("src");
-//     while (link.search("&amp;") != -1) {
-//       link = link.replace("&amp;", "&");
-//     }
-//     let caption =await getCaptionFromHtml(response.data);
-//     return {link,caption};
-//   });
-//   return res;
-// }
+async function getPostLink(url) {
+  url = url + 'embed' + '/captioned';
+  let res = axios.get(url).then(async (response) => {
+    const root = parse(response.data);
+    let videoLink = getVideoLinkFromHtml(response.data);
+    while (videoLink.search("&amp;") != -1) {
+      videoLink = videoLink.replace("&amp;", "&");
+    }
+    let caption =await getCaptionFromHtml(response.data);
+    let img = root.querySelector('img.EmbeddedMediaImage').getAttribute("src");
+    return {videoLink, caption, img};
+  });
+  return res;
+}
 
-// async function getCaptionFromHtml(html) {
-//   const root = parse(html);
-//   let caption=root.querySelector('.Caption')?.text;
-//   if(caption == undefined)
-//       caption="No caption";
-//   caption=caption.replace("view all comments","");
-//   return caption;
-// }
+async function getCaptionFromHtml(html) {
+  const root = parse(html);
+  let caption=root.querySelector('.Caption')?.text;
+  if(caption == undefined)
+      caption="No caption";
+  caption=caption.replace("view all comments","");
+  return caption;
+}
 
-// function getVideoLinkFromHtml(html) {
-//   let crop = "{\"" + html.substring(html.search("video_url"), html.search("video_url") + 1000);
-//   crop = crop.substring(0, crop.search(",")) + "}";
-//   return JSON.parse(crop).video_url;
-// }
+function getVideoLinkFromHtml(html) {
+  let crop = "{\"" + html.substring(html.search("video_url"), html.search("video_url") + 1000);
+  crop = crop.substring(0, crop.search(",")) + "}";
+  return JSON.parse(crop).video_url;
+}
 
-// router.get('/info', async (req, res)=>{
-//     try{
-//         let url = req.query.url;
-//         url= url.replace('?utm_medium=copy_link','');
-//         const data= await getPostLink(url);
-//         console.log(data);
-//         res.json(data);
-//     }catch(err){
-//         console.log(err.message);
-//         res.status(404).send(err.message);
-//     }
-// });
+router.get('/test', async (req, res)=>{
+    try{
+        let url = req.query.url;
+        url= url.replace('?utm_medium=copy_link','');
+        const data= await getPostLink(url);
+        console.log(data);
+        res.json(data);
+    }catch(err){
+        console.log(err.message);
+        res.status(404).send(err.message);
+    }
+});
 
 
 const getInfo = async (url) => {
@@ -66,7 +63,7 @@ const getInfo = async (url) => {
 router.get('/info', async (req, res)=>{
     try{
         let url = req.query.url;
-        url= url.replace('?utm_medium=copy_link','');
+        // url= url.replace('?utm_medium=copy_link','');
         url= url + 'embed' + '/captioned';
         const data = await getInfo(url);
         console.log(data);
