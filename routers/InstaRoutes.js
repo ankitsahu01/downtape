e17970151a0d142c2e1c0ab258cd6726232ab8da
Ankit Sahu <sahu6856@gmail.com>
2021-08-16 15:39:10 +0530

    test

--- routers/InstaRoutes.js
+++ routers/InstaRoutes.js
@@ -3,56 +3,24 @@ const router = express.Router();
 const axios = require("axios");
 const download = require('image-downloader')
 const cheerio = require('cheerio');
-
 const { parse } = require("node-html-parser");
 
-const https = require('https');
-const getContentLength= (url)=>{
-  return new Promise(((resolve, reject) => {
-      const request = https.request(url, (response) => {
-          // console.log(response);
-          response.setEncoding('utf8');
-          // console.log(response);
-          response.on('data', (d)=>{
-            // console.log(d);
-            const clen= response.headers['content-length'];
-            // console.log(clen);
-            // console.log(response);
-            resolve(JSON.parse(clen));
-          })
-          response.on('error', (error) => {
-              throw error;
-          });
-      });
-      request.end();
-  }))
-}
+const Insta = require('scraper-instagram');
+const InstaClient = new Insta();
 
-const getInfo = async (url) => {
-  const html = await axios.get(url);
-  const root = parse(html.data);
-  // console.log(html);
-  let caption2=root.querySelector('.Caption')?.text;
-  console.log(caption2);
-  return html;
-  // const $ = cheerio.load(html.data);
-  // const videoString = $("meta[property='og:video']").attr("content");
-  // const imgString = $("meta[property='og:image']").attr("content");
-  // const caption = $("meta[property='og:title']").attr("content");
-  // return {videoString, imgString, caption};
-};
-
+// const instagramGetUrl = require("instagram-url-direct")
 router.get('/info', async (req, res)=>{
     try{
         let url = req.query.url;
         url= url.replace('?utm_medium=copy_link','');
-        
-        const r= await getContentLength(url);
-        console.log(r);
+        let shortcode= url.split('/');
+        shortcode= shortcode[shortcode.length-2];
+        InstaClient.getPost(shortcode)
+          .then(post => console.log(post))
+          .catch(err => console.error(err));
 
-        // const data = await getInfo(url);
-        // console.log(data);
-        
+        // let links = await instagramGetUrl(url);
+        // console.log(links)
         res.json("ok");
     }catch(err){
         console.log(err);
