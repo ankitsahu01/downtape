cc705f2604a7e1900baca5674bdff9f1b28f4fe7
Ankit Sahu <sahu6856@gmail.com>
2021-08-16 22:45:03 +0530

    test

--- client/src/components/Test.js
+++ client/src/components/Test.js
@@ -3,18 +3,20 @@ import axios from 'axios';
 
 const Test = () => {
   const [url, setUrl]= useState('https://www.instagram.com/p/CB5d063pwo-/');
-  const [data, setData]= useState({ videoLink: '', imgLink: '', caption: '' });
+  const [data, setData]= useState({
+    caption: '',
+    videoLink: '',
+    imgPublicLink: '',
+    views: '',
+    likes: '',
+    commentCount: ''
+  });
 
   const searchVideo= async ()=>{
     try{
       const res= await axios(`/api/insta/info?url=${url}`);
-      let {videoLink, imgName, caption}= res.data;
-      console.log("IMG Name "+imgName);
-      let imgLink= `http://localhost:5000/img/${imgName}`;
-      if(process.env.NODE_ENV==="production"){
-        imgLink= `https://downtape.herokuapp.com/img/${imgName}`;
-      }
-      setData({videoLink, imgLink, caption});
+      let {caption, videoLink, imgPublicLink, views, likes, commentCount}= res.data;
+      setData({caption, videoLink, imgPublicLink, views, likes, commentCount});
     }catch(err){
       console.log(err.message);
     }
@@ -27,16 +29,14 @@ const Test = () => {
      <br/>
      <button onClick={searchVideo}>Search</button>
      <br/>
-     <video controls poster="https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/e35/226533970_991515171608290_8124624967778992359_n.jpg?_nc_ht=scontent-iad3-1.cdninstagram.com&_nc_cat=110&_nc_ohc=XHVBp88-EC8AX__BsGx&edm=AABBvjUBAAAA&ccb=7-4&oh=2e676b12c8551aa25a75c54216fca25c&oe=611C8D9B&_nc_sid=83d603">
-       <source src="https://scontent-iad3-1.cdninstagram.com/v/t50.2886-16/10000000_787499208591130_9223319361261511444_n.mp4?_nc_ht=scontent-iad3-1.cdninstagram.com&_nc_cat=110&_nc_ohc=jAisvksF53EAX-QxCoB&edm=AABBvjUBAAAA&ccb=7-4&oe=611CF10B&oh=e93c55f9a9923216215a98ce49f5683b&_nc_sid=83d603" type="video/mp4" />
-     </video>
-     <img src="https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/e35/226533970_991515171608290_8124624967778992359_n.jpg?_nc_ht=scontent-iad3-1.cdninstagram.com&_nc_cat=110&_nc_ohc=XHVBp88-EC8AX__BsGx&edm=AABBvjUBAAAA&ccb=7-4&oh=2e676b12c8551aa25a75c54216fca25c&oe=611C8D9B&_nc_sid=83d603" />
-     <br/>
      {
        data.videoLink ? 
         <>
-        <img src={data.imgLink} style={{'width':'300px'}} /><br/>
+        <img src={data.imgPublicLink} style={{'width':'300px'}} /><br/>
         <p>{data.caption}</p>
+        <p>Views: {data.views}</p>
+        <p>Likes: {data.likes}</p>
+        <p>Comments: {data.commentCount}</p>
         <a href={data.videoLink}>Download</a>
         </>
        : ''
--- routers/InstaRoutes.js
+++ routers/InstaRoutes.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 const download = require('image-downloader');
 
 const Insta = require('scraper-instagram');
 const InstaClient = new Insta();
+
+const {Dropbox} = require('dropbox');
+const fs= require('fs');
+const accessToken = 'JtKUGjuCPFsAAAAAAAAAAWbJANG9xElJmywUq2NtkabBf515_gu9ST9uuV7fMB8d';
+const dbx = new Dropbox({ accessToken, axios });
+
+const UploadToDbx= (imgName)=>{
+  return new Promise((resolve, reject)=>{
+    fs.readFile(`./backend_img/${imgName}`, async (err, data)=>{
+      try{
+        await dbx.filesUpload({
+          path: `/${imgName}`,
+          contents: data
+        });
+        const shareObj = await dbx.sharingCreateSharedLinkWithSettings({ 
+          path: `/${imgName}`,
+        });
+        const ShareableLink= shareObj.result.url;
+        resolve(ShareableLink);
+      }catch(err){
+        if(err.status===409 && err.error.error['.tag']==='shared_link_already_exists'){
+          resolve(err.error.error.shared_link_already_exists.metadata.url);
+        }else{
+          console.log(err);
+          reject(err);
+        }
+      }
+    });
+  });
+}
 
 router.get('/info', async (req, res)=>{
     try{
@@ -13,13 +44,20 @@ router.get('/info', async (req, res)=>{
       shortcode= shortcode[shortcode.length-2];
       await InstaClient.authBySessionId('49024374510%3AEqCrXsT2J9WYE1%3A11');
       const data = await InstaClient.getPost(shortcode);
-      // console.log(data);
+      
       const videoLink= data.contents[0].url;
-      const imgLink= data.contents[0].thumbnail;
+      const imgPrivateLink= data.contents[0].thumbnail;
       const views= data.contents[0].views;
-      const likes= data.likes;
-      const caption= data.caption;
-      res.status(200).json({videoLink, imgLink, views, likes, caption});
+      const {likes, caption, commentCount}= data;
+
+      const imgDwnld= await download.image({ url: imgPrivateLink, dest: './backend_img' });
+      const imgName= imgDwnld.filename.replace('backend_img\\','');
+      
+      let imgPublicLink= await UploadToDbx(imgName);
+      imgPublicLink= imgPublicLink.replace("?dl=0","?raw=1");
+
+      fs.unlinkSync(`./backend_img/${imgName}`);
+      res.status(200).json({caption, videoLink, imgPublicLink, views, likes, commentCount});
     }
     catch(err){
       if(err===401){
