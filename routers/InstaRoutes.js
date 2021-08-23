96a8733eca7a789701090629199784a36d528831
Ankit Sahu <sahu6856@gmail.com>
2021-08-23 14:06:42 +0530

    test

--- routers/InstaRoutes.js
+++ routers/InstaRoutes.js
@@ -70,19 +70,28 @@ router.get('/info', async (req, res)=>{
 });
 
 
-const instagramGetUrl = require("instagram-url-direct")
 router.get('/test', async (req, res)=>{
   try{
     let url = req.query.url;
-    url= url.replace('?utm_medium=copy_link','');
-    const links = await instagramGetUrl(url);
-    console.log('--------------',links,'-------------------');
-    const videoLink= links.url_list[0];
-    res.status(200).json({videoLink});
+      url= url.replace('?utm_medium=copy_link','');
+      let shortcode= url.split('/');
+      shortcode= shortcode[shortcode.length-2];
+      await InstaClient.authBySessionId('49024374510%3AEqCrXsT2J9WYE1%3A11');
+      const data = await InstaClient.getPost(shortcode);
+      const videoLink= data.contents[0].url;
+      res.status(200).json({videoLink});
   }
   catch(err){
-    console.log(err);
-    res.status(404).json(err);
+    if(err===401){
+      console.log('Invalid Insta SessionId');
+      res.status(404).json({error:"Invalid Insta SessionId"});
+    } else if(err===429){
+      console.log('Too Many Requests');
+      res.status(404).json({error:"Too Many Requests"});
+    }else{
+      console.log(err);
+      res.status(404).json(err);
+    }
   }
 });
 
