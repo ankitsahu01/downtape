e011368d1d7f9fcf6e732c0295510ed40c0a0a27
Ankit Sahu <sahu6856@gmail.com>
2021-08-25 13:56:08 +0530

    test

--- routers/FbRoutes.js
+++ routers/FbRoutes.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const fbvd = require('fb-video-downloader');
+const fbdl = require("fbdl-core");
 
 router.get('/getInfo', async (req, res)=>{
   try{
     const {url}= req.query;
-    const data= await fbvd.getInfo(url);
-    console.log(data);
-    const {hd, sd}= data.download;
-    const {thumb, title}= data;
-    res.status(200).json({hd, sd, thumb, title});
+    fbdl.getInfo(url)
+    .then((data)=>{
+        console.log(data);
+        res.status(200).json({data});
+    }).catch((err)=>{
+        res.status(200).json("testing123");
+    })
   }catch(err){
     console.log(err);
      res.status(404).json({err})
