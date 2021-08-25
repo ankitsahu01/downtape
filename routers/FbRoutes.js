2ed385fb667f811402833f7666d7f8f6b53d9f50
Ankit Sahu <sahu6856@gmail.com>
2021-08-25 16:15:44 +0530

    test

--- routers/FbRoutes.js
+++ routers/FbRoutes.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const fbdl = require("fbdl-core");
 
-const fbvid = require('fbvideos');
+const nodeFbLogin = require('node-fb-login');
 
 router.get('/', async (req, res)=>{
     try{
         // const {url}= req.query;
-        const video= "https://www.facebook.com/zeemusiccompany/videos/531377684265559";
-        const sd= await fbvid.low(video);
-        const hd= await fbvid.high(video);
-        console.log(sd,hd);
-        res.status(200).json({sd:sd.url, hd:hd.url});
+        nodeFbLogin.generateAuthURL({
+            fbAppID: "401349464841869",
+            redirectURI: "downtape.herokuapp.com",
+            scopes:["public_profile","email"]
+        }).then(URL=>{
+            console.log(URL);
+            const url = "https://www.facebook.com/zeemusiccompany/videos/531377684265559";
+            fbdl.getInfo(url).then((data)=>{
+                console.log(data);
+                res.status(200).json(data);
+            })
+        }).catch(error=>{
+            console.log(error);
+        })
     }catch(err){
         res.status(404).json({error:err.message});
         console.log({error:err.message});
