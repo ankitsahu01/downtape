333461535138f0eebe6ff07e69e1e4916356217c
Ankit Sahu <sahu6856@gmail.com>
2021-08-26 19:52:35 +0530

    test

--- routers/TwitterRoutes.js
+++ routers/TwitterRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const twitterGetUrl = require("twitter-url-direct")
+const twitterGetUrl = require("twitter-url-direct");
 
 router.get('/', async (req, res)=>{
     try{
