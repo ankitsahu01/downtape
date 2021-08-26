67c8ab66394249d209be5e3a319aecb852a4ac4d
Ankit Sahu <sahu6856@gmail.com>
2021-08-26 22:22:11 +0530

    text

--- routers/TwitterRoutes.js
+++ routers/TwitterRoutes.js
@@ -2,24 +2,47 @@ const express = require('express');
 const router = express.Router();
 const videoUrlLink = require('video-url-link');
 
-
-router.get('/', async (req, res)=>{
-    try{
-        const url= "https://twitter.com/i/status/1430466398069743617";
-        videoUrlLink.twitter.getInfo(url, {}, (error, info) => {
-            if (error) {
-                console.error(error);
+const https = require('https');
+const getContentLength= (url)=>{
+    return new Promise(((resolve, reject) => {
+        const request = https.request(url, (response) => {
+            const clen= response.headers['content-length'];
+            resolve(JSON.parse(clen));
+            response.on('error', (error) => {
                 throw error;
-            } else {
-                console.log(info.full_text);
-                console.log(info.variants);
-                res.status(200).json(info);
-            }
+            });
         });
-    }catch(err){
-        res.status(404).json({error:err.message});
-        console.log({error:err.message});
-    }
+        request.end();
+    }))
+}
+
+router.get('/', (req, res)=>{
+    const url= "https://twitter.com/i/status/1430466398069743617";
+    videoUrlLink.twitter.getInfo(url, {}, async(error, info) => {
+        try{
+            if(error){
+                throw error;
+            }else{
+                const requiredInfo= {
+                    title: info.full_text,
+                    formats: await Promise.all(
+                        info.variants
+                        .filter((variant)=>{return variant.content_type==="video/mp4"})
+                        .map( async (variant)=>{
+                            return {
+                                'contentLength': await getContentLength(variant.url),
+                                ...variant
+                            }
+                        })
+                    )
+                };
+                res.status(200).json({ requiredInfo});
+            }
+        }catch(err){
+            res.status(404).json({error:err.message});
+            console.log({error:err.message});
+        }
+    });
 });
 
 module.exports = router;
