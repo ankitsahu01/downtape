e6ef2ec3493365a678bb48a3172a6117ad31a918
Ankit Sahu <sahu6856@gmail.com>
2021-08-26 20:00:35 +0530

    test

--- routers/TwitterRoutes.js
+++ routers/TwitterRoutes.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const twitterGetUrl = require("twitter-url-direct");
+const videoUrlLink = require('video-url-link');
+
 
 router.get('/', async (req, res)=>{
     try{
         const url= "https://twitter.com/i/status/1430466398069743617";
-        const result= await twitterGetUrl(url);
-		console.log(result);
-        res.status(200).json(result);
+        videoUrlLink.twitter.getInfo(url, {}, (error, info) => {
+            if (error) {
+                console.error(error);
+                throw error;
+            } else {
+                console.log(info.full_text);
+                console.log(info.variants);
+                res.status(200).json(info);
+            }
+        });
     }catch(err){
         res.status(404).json({error:err.message});
         console.log({error:err.message});
