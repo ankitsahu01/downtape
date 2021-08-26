70cc847971b68abeeae1b350e68a28705646f65b
Ankit Sahu <sahu6856@gmail.com>
2021-08-26 16:19:16 +0530

    test

--- app.js
+++ app.js
@@ -18,5 +18,11 @@ if(process.env.NODE_ENV==="production"){
     });
 }
 
+// const fbdl= require('fbdl-core');
+// const url = "https://www.facebook.com/zeemusiccompany/videos/531377684265559";
+// fbdl.getInfo(url).then(response=>console.log(response)).catch(err=>console.log(err));
+
+
+    
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
--- routers/FbRoutes.js
+++ routers/FbRoutes.js
@@ -2,35 +2,26 @@ const express = require('express');
 const router = express.Router();
 const fbdl = require("fbdl-core");
 const axios = require('axios');
+const puppeteer = require('puppeteer');
 
 router.get('/', async (req, res)=>{
     try{
-        const tokenUrl="https://graph.facebook.com/oauth/access_token?client_id=401349464841869&client_secret=abffa5a8fcce6c9f5285719511a6c9b6&grant_type=client_credentials";
-        const tokenObj= await axios.get(tokenUrl);
-        const token= tokenObj.data.access_token;
-        console.log(token);
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+        await page.setViewport({ width: 1024, height: 480 });
 
-        const scrape = require('scrape-fb-ogcache');
-        const url = "https://www.facebook.com/zeemusiccompany/videos/531377684265559";
-        scrape(url, token, (resp) => {
-            console.log(resp);
-            const url = "https://www.facebook.com/zeemusiccompany/videos/531377684265559";
-            fbdl.getInfo(url).then((data)=>{
-                console.log(data);
-                res.status(200).json(data);
-            }).catch((err)=>{
-                console.log(err);
-                res.status(404).json(err);
-            })
-        });
-        // res.status(200).json(token);
+        await page.goto('https://www.facebook.com/zeemusiccompany/videos/531377684265559', {'timeout': 10000, 'waitUntil':'load'});
+        
+        const data = await page.content();
+        await browser.close();
 
         // const {url}= req.query;
         // const url = "https://www.facebook.com/zeemusiccompany/videos/531377684265559";
-        // const response = await got(url);
+        // const response = await fbdl.getInfo(url);
 		// console.log(response.body);
         // res.status(200).json(response.body);
 
+        res.status(200).json(data);
     }catch(err){
         res.status(404).json({error:err.message});
         console.log({error:err.message});
