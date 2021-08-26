97921f81e1f8d85659153751958bc31b3464eb3b
Ankit Sahu <sahu6856@gmail.com>
2021-08-26 16:40:01 +0530

    test

--- routers/FbRoutes.js
+++ routers/FbRoutes.js
@@ -6,7 +6,12 @@ const puppeteer = require('puppeteer');
 
 router.get('/', async (req, res)=>{
     try{
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+            'args' : [
+              '--no-sandbox',
+              '--disable-setuid-sandbox'
+            ]
+        });
         const page = await browser.newPage();
         await page.setViewport({ width: 1024, height: 480 });
 
