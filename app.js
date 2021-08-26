84fbecebbc302875803bb2dd160a016992aae585
Ankit Sahu <sahu6856@gmail.com>
2021-08-26 16:55:31 +0530

    test

--- app.js
+++ app.js
@@ -17,12 +17,6 @@ if(process.env.NODE_ENV==="production"){
         res.sendFile(path.resolve(__dirname,'client','build','index.html'));
     });
 }
-
-// const fbdl= require('fbdl-core');
-// const url = "https://www.facebook.com/zeemusiccompany/videos/531377684265559";
-// fbdl.getInfo(url).then(response=>console.log(response)).catch(err=>console.log(err));
-
-
     
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
--- routers/FbRoutes.js
+++ routers/FbRoutes.js
@@ -1,32 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const fbdl = require("fbdl-core");
-const axios = require('axios');
-const puppeteer = require('puppeteer');
 
 router.get('/', async (req, res)=>{
     try{
-        const browser = await puppeteer.launch({
-            'args' : [
-              '--no-sandbox',
-              '--disable-setuid-sandbox'
-            ]
-        });
-        const page = await browser.newPage();
-        await page.setViewport({ width: 1024, height: 480 });
-
-        await page.goto('https://www.facebook.com/zeemusiccompany/videos/531377684265559', { 'waitUntil':'load'});
-        
-        const data = await page.content();
-        await browser.close();
-
-        // const {url}= req.query;
-        // const url = "https://www.facebook.com/zeemusiccompany/videos/531377684265559";
-        // const response = await fbdl.getInfo(url);
-		// console.log(response.body);
-        // res.status(200).json(response.body);
-
-        res.status(200).json(data);
+        const {url}= req.query;
+        const url = "https://www.facebook.com/zeemusiccompany/videos/531377684265559";
+        const response = await fbdl.getInfo(url);
+		console.log(response.body);
+        res.status(200).json(response.body);
     }catch(err){
         res.status(404).json({error:err.message});
         console.log({error:err.message});
