04e94d677d6a728b22f97c5495fbf68675c17cbc
Ankit Sahu <sahu6856@gmail.com>
2021-08-31 18:21:35 +0530

    test

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+var expressStaticGzip = require("express-static-gzip");
 const path = require('path');
 const app = express();
 
@@ -14,6 +15,11 @@ app.use('/api/test', require('./routers/TestRoutes'));
 
 
 if(process.env.NODE_ENV==="production"){
+    app.use(
+        expressStaticGzip(path.resolve(__dirname,'client','build'), {
+            enableBrotli: true, // only if you have brotli files too
+        }),
+    );
     app.use(express.static(path.resolve(__dirname,'client','build')));
     app.get('/*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'));
