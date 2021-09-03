b2bfb6522db26f695c7d067247c5287ab97b25e8
Ankit Sahu <sahu6856@gmail.com>
2021-09-03 19:53:37 +0530

    test

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import { CssBaseline, ThemeProvider } from "@material-ui/core";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import theme from "./theme";
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,8 +16,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
