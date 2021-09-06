67f25a64189bec720f49b5be46e4a29980787d1d
Ankit Sahu <sahu6856@gmail.com>
2021-09-06 14:49:53 +0530

    test

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {NavLink} from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import Drawer from "@material-ui/core/Drawer";
-import {AppBar, Toolbar, Typography, Button} from '@material-ui/core';
+import {AppBar, Toolbar, Typography} from '@material-ui/core';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import {Fab, Slide, Zoom} from '@material-ui/core';
-import YouTubeIcon from "@material-ui/icons/YouTube";
-import InstagramIcon from "@material-ui/icons/Instagram";
-import TwitterIcon from "@material-ui/icons/Twitter";
-import { SvgIcon } from "@material-ui/core";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles((theme) => ({
   scrollTop: {
@@ -26,36 +19,6 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.light.main,
     textDecoration: "none",
   },
-  navlink: {
-    color: theme.palette.light.main,
-    textDecoration: "none",
-    borderRadius: '0',
-    padding: theme.spacing(2),
-    marginRight: theme.spacing(4),
-    '&:hover': {
-      // borderBottom: `1px solid ${theme.palette.light.main}`,
-      borderBottom: "1px solid",
-    },
-  },
-  MobNavlink: {
-    color: theme.palette.light.main,
-    textDecoration: "none",
-    borderRadius: '0',
-    marginTop: theme.spacing(2),
-  },
-  activeNavLink: {
-    // borderBottom: `2px solid ${theme.palette.light.main}`,
-    borderBottom: "2px solid",
-    '&:hover': {
-      borderBottom: "2px solid",
-    },
-  },
-  grbg:{
-    background: theme.palette.navGr.main,
-  },
-  paper: {
-    background: theme.palette.navGr.main,
-  }
 }));
 
 const ScrollTop=(props)=> {
@@ -93,128 +56,7 @@ const HideOnScroll=(props)=> {
 
 const Navbar=(props)=> {
   const classes = useStyles();
-  const [mobileView, setMobileView] = useState(false);
-  const [openDrawer, setOpenDrawer] = useState(false);
-  const toggleDrawer = (updateOpen) => (event) => {
-    if ( event.type === "keydown" && (event.key === "Tab" || event.key === "Shift") ) {
-      return;
-    }
-    setOpenDrawer(updateOpen);
-  };
   
-  useEffect(()=>{
-    const setResponsiveness = ()=>{
-      window.innerWidth < 900 
-        ? setMobileView(true)
-        : setMobileView(false)
-    };
-    setResponsiveness();
-    window.addEventListener('resize', ()=>setResponsiveness());
-    return ()=>{
-      window.removeEventListener('resize', ()=>setResponsiveness());
-    }
-  },[mobileView]);
-
-  const DesktopDisplay = ()=>{
-    return (
-      <>
-        <Button color="inherit" component={NavLink}
-            to="/youtube-video-downloader"
-            className={classes.navlink}
-            activeClassName={classes.activeNavLink}
-          >
-            <YouTubeIcon fontSize="large" />
-        </Button>
-        <Button color="inherit" component={NavLink}
-            to="/instagram-video-downloader"
-            className={classes.navlink}
-            activeClassName={classes.activeNavLink}
-          >
-            <InstagramIcon fontSize="large" />
-          </Button>
-        <Button color="inherit" component={NavLink}
-            to="/twitter-video-downloader"
-            className={classes.navlink}
-            activeClassName={classes.activeNavLink}
-          >
-            <TwitterIcon fontSize="large" />
-          </Button>
-          <Button color="inherit" component={NavLink}
-            to="/vimeo-video-downloader"
-            className={classes.navlink}
-            activeClassName={classes.activeNavLink}
-          >
-            <SvgIcon fontSize="large">
-              <path
-                d="M17.811,2.018c2.017,0.053,3.026,1.198,3.036,3.438c0,0.147-0.005,0.3-0.013,0.457c-0.089,1.899-1.502,4.486-4.245,7.76
-                  c-2.829,3.43-5.229,5.147-7.2,5.156c-1.226,0-2.244-1.05-3.061-3.151l-0.858-2.88L4.622,9.922C3.997,7.838,3.329,6.798,2.616,6.798
-                  c-0.156,0-0.697,0.304-1.626,0.91L0,6.537l1.536-1.276l1.511-1.263C4.4,2.914,5.429,2.328,6.135,2.241
-                  c0.094-0.01,0.188-0.013,0.284-0.013c1.449,0,2.354,1.041,2.709,3.124C9.326,6.54,9.49,7.506,9.623,8.248
-                  C9.752,8.992,9.86,9.51,9.946,9.805c0.479,1.97,0.995,2.96,1.55,2.968c0.426,0,1.082-0.642,1.968-1.926
-                  c0.866-1.319,1.332-2.296,1.392-2.932c0.019-0.129,0.026-0.25,0.026-0.362c0-0.861-0.474-1.29-1.418-1.29
-                  c-0.479,0-0.99,0.102-1.537,0.299c0.98-3.021,2.864-4.534,5.65-4.544C17.655,2.018,17.732,2.018,17.811,2.018z"
-              />
-            </SvgIcon>
-        </Button>
-      </>
-    );
-  };
-
-  const MobileDisplay= ()=>{
-    return(
-      <>
-      <IconButton
-        color="inherit"
-        aria-label="open drawer"
-        edge="end"
-        onClick={toggleDrawer(true)}
-      >
-        <MenuIcon />
-      </IconButton>
-      <Drawer anchor="right" open={openDrawer} onClose={toggleDrawer(false)} classes={{ paper: classes.paper }}>
-        <Button onClick={toggleDrawer(false)} component={NavLink}
-            to="/youtube-video-downloader"
-            className={classes.MobNavlink}
-            activeClassName={classes.activeNavLink}
-          >
-            <YouTubeIcon fontSize="large" />
-        </Button>
-        <Button onClick={toggleDrawer(false)} component={NavLink}
-            to="/instagram-video-downloader"
-            className={classes.MobNavlink}
-            activeClassName={classes.activeNavLink}
-          >
-            <InstagramIcon fontSize="large" />
-        </Button>
-        <Button onClick={toggleDrawer(false)} component={NavLink}
-            to="/twitter-video-downloader"
-            className={classes.MobNavlink}
-            activeClassName={classes.activeNavLink}
-          >
-            <TwitterIcon fontSize="large" />
-        </Button>
-        <Button onClick={toggleDrawer(false)} component={NavLink}
-            to="/vimeo-video-downloader"
-            className={classes.MobNavlink}
-            activeClassName={classes.activeNavLink}
-          >
-            <SvgIcon fontSize="large">
-              <path
-                d="M17.811,2.018c2.017,0.053,3.026,1.198,3.036,3.438c0,0.147-0.005,0.3-0.013,0.457c-0.089,1.899-1.502,4.486-4.245,7.76
-                c-2.829,3.43-5.229,5.147-7.2,5.156c-1.226,0-2.244-1.05-3.061-3.151l-0.858-2.88L4.622,9.922C3.997,7.838,3.329,6.798,2.616,6.798
-                c-0.156,0-0.697,0.304-1.626,0.91L0,6.537l1.536-1.276l1.511-1.263C4.4,2.914,5.429,2.328,6.135,2.241
-                c0.094-0.01,0.188-0.013,0.284-0.013c1.449,0,2.354,1.041,2.709,3.124C9.326,6.54,9.49,7.506,9.623,8.248
-                C9.752,8.992,9.86,9.51,9.946,9.805c0.479,1.97,0.995,2.96,1.55,2.968c0.426,0,1.082-0.642,1.968-1.926
-                c0.866-1.319,1.332-2.296,1.392-2.932c0.019-0.129,0.026-0.25,0.026-0.362c0-0.861-0.474-1.29-1.418-1.29
-                c-0.479,0-0.99,0.102-1.537,0.299c0.98-3.021,2.864-4.534,5.65-4.544C17.655,2.018,17.732,2.018,17.811,2.018z"
-              />
-            </SvgIcon>
-        </Button>
-      </Drawer>
-    </>
-    )
-  };
-
   return (
     <>
       <HideOnScroll {...props}>
@@ -225,7 +67,6 @@ const Navbar=(props)=> {
                 DOWNTAPE
               </NavLink>
             </Typography>
-            { mobileView ? <MobileDisplay/> : <DesktopDisplay/> }
           </Toolbar>
         </AppBar>
       </HideOnScroll>
