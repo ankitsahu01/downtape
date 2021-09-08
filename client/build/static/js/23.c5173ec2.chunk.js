d8c94fe0647a711595cb1ad3a93297fee38d7e88
Ankit Sahu <sahu6856@gmail.com>
2021-09-08 14:32:45 +0530

    optimized performance

--- client/build/static/js/23.c5173ec2.chunk.js
+++ client/build/static/js/23.c5173ec2.chunk.js
@@ -1,2 +1,1 @@
 (this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[23],{223:function(e,t,n){"use strict";n.r(t);var c=n(2);t.default=function(){return Object(c.jsx)(c.Fragment,{children:Object(c.jsx)("h1",{children:"Test Mode"})})}}}]);
-//# sourceMappingURL=23.c5173ec2.chunk.js.map
